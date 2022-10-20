18442a75980965a651025bc4c3f5b4fd0cfb3a94
khalshehri <44474746+khalshehri@users.noreply.github.com>
2022-10-20 21:42:27 +0300

    last change

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -17,7 +17,6 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 
 @app.route('/')
 @app.route('/home')
-@app.route('/getAToken')
 @login_required
 def home():
     user = User.query.filter_by(username=current_user.username).first_or_404()
@@ -67,6 +66,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
+            app.logger.error('Invalid username or password')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
@@ -83,12 +83,17 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
+        app.logger.error('Authentication/Authorization failure')
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(request.args['code'], scopes=Config.SCOPE, redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
+     request.args['code'],
+     scopes=Config.SCOPE,
+     redirect_uri=url_for('authorized', _external=True, _scheme='https'))
         if "error" in result:
+            app.logger.error('Authentication/Authorization failure')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
@@ -96,6 +101,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
+        app.logger.error('admin logged successfully')
     return redirect(url_for('home'))
 
 @app.route('/logout')
@@ -111,6 +117,19 @@ def logout():
 
     return redirect(url_for('login'))
 
+@app.route("/getAToken")
+def main_logic():
+    code = request.args['code']
+    state = request.args['state']
+    if state != session['state']:
+        raise ValueError("State does not match")
+    auth_context = adal.AuthenticationContext(AUTHORITY_URL)
+    token_response = auth_context.acquire_token_with_authorization_code(code, REDIRECT_URI, config.RESOURCE,
+                                                                        config.CLIENT_ID, config.CLIENT_SECRET)
+    Flask.session['access_token'] = token_response['accessToken']
+
+    return Flask.redirect('/index')
+    
 def _load_cache():
     cache = msal.SerializableTokenCache()
     if session.get('token_cache'):
@@ -124,12 +143,12 @@ def _save_cache(cache):
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
     return msal.ConfidentialClientApplication(
-        Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
-        client_credential=Config.CLIENT_SECRET, token_cache=cache)
+    Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
+    client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
     return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes or [],
-        state=state or str(uuid.uuid4()),
-        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    scopes or [],
+    state=state or str(uuid.uuid4()),
+    redirect_uri=url_for('authorized', _external=True, _scheme='https'))
--- WRITEUP.md
+++ WRITEUP.md
@@ -2,11 +2,13 @@
 
 ### Analyze, choose, and justify the appropriate resource option for deploying the app.
 
-*For **both** a VM or App Service solution for the CMS app:*
-- *Analyze costs, scalability, availability, and workflow*
-- *Choose the appropriate solution (VM or App Service) for deploying the app*
-- *Justify your choice*
+I will deploy my application in App Service insted of VM because : 
+ - Azure web service is more cheaper than VM.
+ - Azure web service need less effort Compared to VM need some effort to manage security , enviroment ... etc
+ - Azure web service easy to deploy by connect to github
+ - Azure web service High availability, auto-scaling, and support of both Linux and Windows environments.
+
 
 ### Assess app changes that would change your decision.
 
-*Detail how the app and any other needs would have to change for you to change your decision in the last section.* 
+For this project and current situation i will use app service, but if scope of my project is expand and i need add some plugin or service using some language not supported by azure web server i will choose the VM.
